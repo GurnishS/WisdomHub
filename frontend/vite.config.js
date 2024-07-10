9b085b0351c1a55c223a55ba29463cf7846d3514
GurnishS <2023kucp1082@iiitkota.ac.in>
2024-07-10 11:55:57 +0530

    server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "https://wisdomhubfrontend.onrender.com/",
+    host: "0.0.0.0",
     port: 5173,
   },
 });
