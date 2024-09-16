79e89eb341e3f505d0e810fe2f857b03bf37c4f3
Gurnish <2023kucp1082@iiitkota.ac.in>
2024-09-16 18:20:54 +0530

    action test

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,5 +8,6 @@ dotenv.config({
 connectDB().then(() => {
   app.listen(process.env.PORT, () => {
     console.log("Server running on port:", process.env.PORT);
+    console.log("Working Fine");
   });
 });
