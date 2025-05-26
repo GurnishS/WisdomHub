c04e434ee2e0fef6716600f98fff2aa5deaabe3d
GurnishS <2023kucp1082@iiitkota.ac.in>
2025-05-26 13:52:24 +0530

    added firebase cred

--- backend/src/utils/firebaseAdmin.js
+++ backend/src/utils/firebaseAdmin.js
@@ -1,8 +1,8 @@
 import admin from 'firebase-admin';
-import fs from 'fs';
-import path from 'path';
-const serviceAccountPath = path.resolve('./firebaseAdmin.json');
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_ADMIN_CREDENTIALS_BASE64, 'base64').toString('utf8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
